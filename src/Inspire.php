6d9e8b68d936df1277dd996386123c5ac8f55053
yusef <yusef22@gmail.com>
2025-07-27 11:16:38 +0330

    Replace external quote API with local Hafez poems

--- src/Inspire.php
+++ src/Inspire.php
@@ -2,12 +2,27 @@
 
 namespace Yusef22\Inspire;
 
-use Illuminate\Support\Facades\Http;
-
 class Inspire {
-    public function justDoIt() {
-        $response = Http::get('https://inspiration.goprogram.ai/');
+    /**
+     * Collection of Hafez poems used for random selection.
+     *
+     * @var array<int, string>
+     */
+    protected array $poems = [
+        'الا یا ایها الساقی ادر کاسا و ناولها که عشق آسان نمود اول ولی افتاد مشکل‌ها',
+        'دوش دیدم که ملائک در میخانه زدند گل آدم بسرشتند و به پیمانه زدند',
+        'منم که شهره شهرم به عشق ورزیدن منم که دیده نیالوده‌ام به بد دیدن',
+        'حافظا می خور و رندی کن و خوش باش ولی دام تزویر مکن چون دگران قرق کنی',
+        'چو بشنوی سخن اهل دل مگو که خطاست سخن شناس نه‌ای جان من خطا اینجا است',
+    ];
 
-        return $response['quote'] . ' -' . $response['author'];
+    /**
+     * Return a random verse from Hafez.
+     */
+    public function justDoIt(): string
+    {
+        $poem = $this->poems[array_rand($this->poems)];
+
+        return $poem . ' - حافظ';
     }
 }
